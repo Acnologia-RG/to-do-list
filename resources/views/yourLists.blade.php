2b97b347ab8e156af9bcdfb9ae1e46831919bee0
rens <rens@gouman.eu>
2020-11-10 14:27:16 +0100

    yourlist updated
    fix a small mistake

--- resources/views/yourLists.blade.php
+++ resources/views/yourLists.blade.php
@@ -3,30 +3,36 @@
 
 <div class="container">
 	<div class="row">
+		<div class="col-md-12">
+		@if (empty($lists[0]))
+			<p>you dont seem to have any lists yet</p>
+		@endif
+
 		@foreach ($lists as $list)
 		<div class="col-md-3" style="border-style:solid; border-color:red;">
 			<h2>{{$list->name}}</h2>
 			<a href="{{url('/')}}/newListItem/{{$list->id}}">New list item</a>
 			@foreach ($listItems as $listItem)
 			@if ($listItem->List_id == $list->id)
-			<div>
+			<div style="border-style:solid; border-color:red;">
 			<p>{{$listItem->name}}</p>
 			<p>{{$listItem->description}}</p>
-			<input type="checkbox" ><label for="status">DONE</label>
+			
+			<input type="checkbox" @if ($listItem->status == 1)
+			checked
+			@endif
+			><label for="status">DONE</label>
+			
 			</div>
 			@endif
 			@endforeach
 		</div>
 		@endforeach
-
 		
-		<div class="col-md-12">
-		@if (!empty($lists))
-			<p>you dont seem to have any lists yet</p>
-		@endif
 		<a class="nav-link" href="{{ url('/newList') }}">make a new list</a>
 		</div>
 	</div>
 </div>
 
+<script src="{{ asset('js/check.js') }}" defer></script>
 @endsection
