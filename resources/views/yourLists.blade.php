4c8c9c076f38e763c6221eff322dc3c23a3703dc
rens <rens@gouman.eu>
2020-11-10 14:54:58 +0100

    last fix i hope
    removed one div that was to much
    fixed spacing and indents
    temp comment the script that does nothing yet

--- resources/views/yourLists.blade.php
+++ resources/views/yourLists.blade.php
@@ -11,27 +11,29 @@
 		<div class="col-3" style="border-style:solid; border-color:red;">
 			<h2>{{$list->name}}</h2>
 			<a href="{{url('/')}}/newListItem/{{$list->id}}">New list item</a>
+			
 			@foreach ($listItems as $listItem)
 			@if ($listItem->List_id == $list->id)
-			<div style="border-style:solid; border-color:red;">
-			<p>{{$listItem->name}}</p>
-			<p>{{$listItem->description}}</p>
-			
-			<input type="checkbox" @if ($listItem->status == 1)
-			checked
-			@endif
-			><label for="status">DONE</label>
-			
-			</div>
+				<div style="border-style:solid; border-color:red;">
+				<p>{{$listItem->name}}</p>
+				<p>{{$listItem->description}}</p>
+				
+				<input type="checkbox" @if ($listItem->status == 1)
+				checked
+				@endif
+				><label for="status">DONE</label>
+				
+				</div>
 			@endif
 			@endforeach
 		</div>
 		@endforeach
 		
-		</div>
-		<a class="nav-link" href="{{ url('/newList') }}">make a new list</a>
 	</div>
+	<a class="nav-link" href="{{ url('/newList') }}">make a new list</a>
 </div>
 
+<!--
 <script src="{{ asset('js/check.js') }}" defer></script>
+-->
 @endsection
